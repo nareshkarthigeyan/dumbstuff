f6545c3ae9e43f6efa622204944069e3158dd7de
Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2024-10-29 19:55:54 +0530

    bogo graph average

--- bogo.py
+++ bogo.py
@@ -1,9 +1,33 @@
+import matplotlib.pyplot as plt
 import random
-arr = [0, 5, 9, -4, 3]
-i = 0;
-while True:
-    i += 1;
-    random.shuffle(arr)
-    if arr == sorted(arr):
-        break;
-print(i)
+
+def bogosort(arr):
+    sorted_array = sorted(arr)
+    it = 0
+    while arr != sorted_array:
+        random.shuffle(arr)
+        it += 1
+    return it
+
+maxTrials = 10
+maxSize = 10
+
+avgBogoSortTimes = []
+
+for i in range(maxSize):
+    sort_times = []
+    for j in range(maxTrials):
+        arr = random.sample(range(100), i)
+        sort_times.append(bogosort(arr))
+    avgBogoSortTimes.append(sum(sort_times) / len(sort_times))
+
+plt.plot(range(1, maxSize + 1), avgBogoSortTimes, 
+marker='o', linestyle='-', color='b')
+plt.title("Bogosort Times for Increasing Array Sizes")
+plt.xlabel("Array Size")
+plt.ylabel("Iterations")
+plt.grid()
+plt.show()
+
+
+
