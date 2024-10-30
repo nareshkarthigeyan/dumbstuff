a7b8d560683b2cb40addda29857e02b825895a15
Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2024-10-30 10:27:06 +0530

    new sorting algos added

--- sortingWC/sortWC.cpp
+++ sortingWC/sortWC.cpp
@@ -19,7 +19,7 @@ class Algorithm {
     }
 
     void display(){
-        cout << "Name: " << name << "\nFastest Time: " << fastest << "\nSlowest Time: " << slowest << "\nCurrent Time: " << current << endl;
+        cout << "Name: " << name << "\nFastest Time: " << fastest / 1000 << "s" << "\nSlowest Time: " << slowest / 1000 << "s" << "\nCurrent Time: " << current / 1000 << "s" << endl;
     }
 };
 
@@ -150,6 +150,181 @@ class MergeSort : public Algorithm {
     }
 };
 
+class InsertionSort : public Algorithm {
+    public:
+    InsertionSort() : Algorithm("Insertion Sort") {};
+
+    void sort(vector<int> a){
+        auto start = chrono::high_resolution_clock::now();
+
+        //Algorithm: 
+        int size = a.size();
+        for (int i = 1; i < size; i++){
+            int key = a[i];
+            int j = i - 1;
+
+            while(j-->= 0 && a[j] > key){
+                a[j + 1] = a[j];
+            }
+            a[j + 1] = key;
+        }
+
+        
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<double, milli> duration = end - start;
+        double time = duration.count();
+        current = time;
+        if (time < fastest) fastest = time;
+        if (time > slowest) slowest = time;
+    }
+};
+class CycleSort : public Algorithm {
+    public:
+    CycleSort() : Algorithm("Cycle Sort") {};
+
+    void sort(vector<int> a){
+        auto start = chrono::high_resolution_clock::now();
+        int n = a.size();
+        int writes = 0;
+    
+        for (int cycle_start = 0; cycle_start <= n - 2; cycle_start++) {
+            int item = a[cycle_start];
+            int pos = cycle_start;
+            for (int i = cycle_start + 1; i < n; i++)
+                if (a[i] < item)
+                    pos++;
+            if (pos == cycle_start)
+                continue;
+            while (item == a[pos])
+                pos += 1;
+
+            if (pos != cycle_start) {
+                swap(item, a[pos]);
+                writes++;
+            }
+
+            while (pos != cycle_start) {
+                pos = cycle_start;
+    
+                for (int i = cycle_start + 1; i < n; i++)
+                    if (a[i] < item)
+                        pos += 1;
+    
+                while (item == a[pos])
+                    pos += 1;
+    
+                if (item != a[pos]) {
+                    swap(item, a[pos]);
+                    writes++;
+                }
+            }
+        }
+
+        
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<double, milli> duration = end - start;
+        double time = duration.count();
+        current = time;
+        if (time < fastest) fastest = time;
+        if (time > slowest) slowest = time;
+    }
+};
+
+class QuickSort : public Algorithm {
+    private:
+    int partition(vector<int>& arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[high]);  
+        return i + 1;
+    }
+
+    void quickSort(vector<int>& arr, int low, int high) { 
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public:
+    QuickSort() : Algorithm("Quick Sort") {};
+
+
+    void sort(vector<int> a){
+        auto start = chrono::high_resolution_clock::now();
+
+        //Algorithm: 
+        int size = a.size();
+        quickSort(a, 0, size - 1);
+
+        
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<double, milli> duration = end - start;
+        double time = duration.count();
+        current = time;
+        if (time < fastest) fastest = time;
+        if (time > slowest) slowest = time;
+    }
+};
+
+class HeapSort : public Algorithm {
+    private:
+    void heapify(vector<int>& arr, int n, int i){
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+        if (largest != i) {
+            swap(arr[i], arr[largest]);
+            heapify(arr, n, largest);
+        }
+    }
+
+    void heapSort(vector<int>& arr){
+        int n = arr.size();
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+        for (int i = n - 1; i > 0; i--) {
+            swap(arr[0], arr[i]);
+            heapify(arr, i, 0);
+        }
+    }
+
+
+    public:
+    HeapSort() : Algorithm("Heap Sort") {};
+
+
+    void sort(vector<int> a){
+        auto start = chrono::high_resolution_clock::now();
+
+        //Algorithm: 
+        int size = a.size();
+        heapSort(a);
+
+        
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<double, milli> duration = end - start;
+        double time = duration.count();
+        current = time;
+        if (time < fastest) fastest = time;
+        if (time > slowest) slowest = time;
+    }
+};
+
+
+
+
 void readFile(const string& filename, vector<int>& arr){
     ifstream file(filename);
     if(!file.is_open()){
@@ -165,27 +340,34 @@ void readFile(const string& filename, vector<int>& arr){
 
 int main(void){
     vector<int> arr;
-    cout << arr.size();
-
     string filename = "input.txt";
-    cout << "reading from file:";
     readFile(filename, arr);
-    cout << endl << "done" << endl;
 
-    cout << arr.size();
+    MergeSort ms;
+    ms.sort(arr);
+    ms.display();
 
-    cout << "bubble sort: " << endl;
     BubbleSort bs;
     bs.sort(arr);
     bs.display();
 
-    cout << "bubble sort: " << endl;
     SelectionSort ss;
     ss.sort(arr);
     ss.display();
 
-    cout << "bubble sort: " << endl;
-    MergeSort ms;
-    ms.sort(arr);
-    ms.display();
+    InsertionSort is;
+    is.sort(arr);
+    is.display();
+
+    QuickSort qs;
+    qs.sort(arr);
+    qs.display();
+
+    HeapSort hs;
+    hs.sort(arr);
+    hs.display();
+
+    CycleSort cs;
+    cs.sort(arr);
+    cs.display();
 }
